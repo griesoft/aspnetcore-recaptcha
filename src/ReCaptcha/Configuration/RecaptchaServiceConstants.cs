bfdd092dfe0f8037c95268cdb9905797893278bf
Joonas Griesinger <griesinger.joonas@gmail.com>
2020-02-16 17:58:16 +0200

    Change token key name to g-recaptcha-response, because this is the name the automatic recaptcha process is adding to the form.

--- src/ReCaptcha/Configuration/RecaptchaServiceConstants.cs
+++ src/ReCaptcha/Configuration/RecaptchaServiceConstants.cs
@@ -6,7 +6,7 @@ namespace GSoftware.AspNetCore.ReCaptcha.Configuration
     internal class RecaptchaServiceConstants
     {
         internal const string GoogleRecaptchaEndpoint = "https://www.google.com/recaptcha/api/siteverify";
-        internal const string TokenKeyName = "Recaptcha-Token";
+        internal const string TokenKeyName = "G-Recaptcha-Response";
         internal const string SettingsSectionKey = "RecaptchaSettings";
     }
 }
