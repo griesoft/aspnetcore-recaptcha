d5fef5dab5541c35296c3716780e56e89a5af1fa
Joonas Griesinger <griesinger.joonas@gmail.com>
2022-04-20 14:34:15 +0300

    Adding a reCAPTCHA V3 tag. This can be used to automatically bind the challenge to a button.

---
+++ src/ReCaptcha/TagHelpers/RecaptchaV3TagHelper.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Text.Encodings.Web;
+using Griesoft.AspNetCore.ReCaptcha.Configuration;
+using Griesoft.AspNetCore.ReCaptcha.Extensions;
+using Griesoft.AspNetCore.ReCaptcha.Localization;
+using Microsoft.AspNetCore.Mvc.Razor.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.Extensions.Options;
+
+namespace Griesoft.AspNetCore.ReCaptcha.TagHelpers
+{
+    /// <summary>
+    /// A reCAPTCHA V3 tag helper, which can be used to automatically bind the challenge to a button.
+    /// </summary>
+    /// <remarks>
+    /// The <see cref="FormId"/> is required. With the exception that you set a <see cref="Callback"/> instead.
+    /// When setting both the value set to <c>Callback</c> always wins and the <c>FormId</c> value is basically irrelevant.
+    /// 
+    /// For easiest use of this tag helper set only the <c>FormId</c>. This will add a default callback function to the body. That function does
+    /// submit the form after a successful reCAPTCHA challenge.
+    /// 
+    /// If the tag is not inside the form that is going to be submitted, you should use a custom callback function. The default callback function
+    /// does not add the reCAPTCHA token to the form, which will result in response verification failure.
+    /// </remarks>
+    /// <example>
+    /// The simplest use of the tag would be:
+    /// <code>
+    /// <recaptchav3 formid="myForm" action="submit">Submit</recaptchav3>
+    /// </code>
+    /// 
+    /// Which will translate into the following HTML:
+    /// <code>
+    /// <button class="g-recaptcha" data-sitekey="your_site_key" data-callback='submitmyForm' data-action='submit'>Submit</button>
+    /// </code>
+    /// </example>
+    [HtmlTargetElement(Attributes = "callback,action")]
+    [HtmlTargetElement(Attributes = "formid,action")]
+    public class RecaptchaV3TagHelper : TagHelper
+    {
+        private readonly ITagHelperComponentManager _tagHelperComponentManager;
+        private readonly RecaptchaSettings _settings;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="settings"></param>
+        /// <param name="tagHelperComponentManager"></param>
+        /// <exception cref="ArgumentNullException"></exception>
+        public RecaptchaV3TagHelper(IOptionsMonitor<RecaptchaSettings> settings, ITagHelperComponentManager tagHelperComponentManager)
+        {
+            _ = settings ?? throw new ArgumentNullException(nameof(settings));
+            _ = tagHelperComponentManager ?? throw new ArgumentNullException(nameof(tagHelperComponentManager));
+
+            _settings = settings.CurrentValue;
+            _tagHelperComponentManager = tagHelperComponentManager;
+        }
+
+        /// <summary>
+        /// The id of the form that will be submitted after a successful reCAPTCHA challenge. 
+        /// </summary>
+        /// <remarks>This does only apply when not specifying a <see cref="Callback"/>.</remarks>
+        public string? FormId { get; set; }
+
+        /// <summary>
+        /// Set the name of your callback function, which is called when the reCAPTCHA challenge was successful. 
+        /// A "g-recaptcha-response" token is added to your callback function parameters for server-side verification.
+        /// </summary>
+        public string Callback { get; set; } = string.Empty;
+
+        /// <summary>
+        /// The name of the action that was triggered.
+        /// </summary>
+        /// <remarks>You should verify that the server-side verification response returns the same action.</remarks>
+        public string Action { get; set; } = string.Empty;
+
+        /// <inheritdoc />
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="NullReferenceException">Thrown when both <see cref="Callback"/> and <see cref="FormId"/> or <see cref="Action"/> are/is null or empty.</exception>
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            _ = output ?? throw new ArgumentNullException(nameof(output));
+
+            if (string.IsNullOrEmpty(Callback) && string.IsNullOrEmpty(FormId))
+            {
+                throw new NullReferenceException(Resources.CallbackPropertyNullErrorMessage);
+            }
+
+            if (string.IsNullOrEmpty(Action))
+            {
+                throw new NullReferenceException(Resources.ActionPropertyNullErrorMessage);
+            }
+
+            if (string.IsNullOrEmpty(Callback))
+            {
+                Callback = $"submit{FormId}";
+                _tagHelperComponentManager.Components.Add(new CallbackScriptTagHelperComponent(FormId!));
+            }
+
+            output.TagMode = TagMode.StartTagAndEndTag;
+
+            output.TagName = "button";
+            output.AddClass("g-recaptcha", HtmlEncoder.Default);
+
+            output.Attributes.SetAttribute("data-sitekey", _settings.SiteKey);
+            output.Attributes.SetAttribute("data-callback", Callback);
+            output.Attributes.SetAttribute("data-action", Action);
+        }
+    }
+}
---
+++ tests/ReCaptcha.Tests/TagHelpers/RecaptchaV3TagHelperTests.cs
@@ -0,0 +1,383 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Griesoft.AspNetCore.ReCaptcha.Configuration;
+using Griesoft.AspNetCore.ReCaptcha.TagHelpers;
+using Microsoft.AspNetCore.Mvc.Razor.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.Extensions.Options;
+using Moq;
+using NUnit.Framework;
+
+namespace ReCaptcha.Tests.TagHelpers
+{
+    [TestFixture]
+    public class RecaptchaV3TagHelperTests
+    {
+        private const string SiteKey = "unit_test_site_key";
+
+        private Mock<IOptionsMonitor<RecaptchaSettings>> _settingsMock;
+        private Mock<ITagHelperComponentManager> _tagHelperComponentManagerMock;
+        private Mock<ICollection<ITagHelperComponent>> _tagHelperComponentCollectionMock;
+        private TagHelperOutput _tagHelperOutputStub;
+        private TagHelperContext _contextStub;
+
+        [SetUp]
+        public void Initialize()
+        {
+            _settingsMock = new Mock<IOptionsMonitor<RecaptchaSettings>>();
+            _settingsMock.SetupGet(instance => instance.CurrentValue)
+                .Returns(new RecaptchaSettings()
+                {
+                    SiteKey = SiteKey,
+                    SecretKey = string.Empty
+                })
+                .Verifiable();
+
+            _tagHelperComponentCollectionMock = new Mock<ICollection<ITagHelperComponent>>();
+            _tagHelperComponentCollectionMock.Setup(instance => instance.Add(It.IsAny<CallbackScriptTagHelperComponent>()))
+                .Verifiable();
+
+            _tagHelperComponentManagerMock = new Mock<ITagHelperComponentManager>();
+            _tagHelperComponentManagerMock.SetupGet(instance => instance.Components)
+                .Returns(_tagHelperComponentCollectionMock.Object);
+
+            _tagHelperOutputStub = new TagHelperOutput("recaptchav3",
+                new TagHelperAttributeList(), (useCachedResult, htmlEncoder) =>
+                {
+                    var tagHelperContent = new DefaultTagHelperContent();
+                    tagHelperContent.SetContent(string.Empty);
+                    return Task.FromResult<TagHelperContent>(tagHelperContent);
+                });
+
+            _contextStub = new TagHelperContext(new TagHelperAttributeList(),
+                new Dictionary<object, object>(),
+                Guid.NewGuid().ToString("N"));
+        }
+
+        [Test]
+        public void Construction_IsSuccessful()
+        {
+            // Arrange
+
+
+            // Act
+            var instance = new RecaptchaV3TagHelper(_settingsMock.Object, _tagHelperComponentManagerMock.Object);
+
+            // Assert
+            Assert.NotNull(instance);
+            _settingsMock.Verify(settings => settings.CurrentValue, Times.Once);
+        }
+
+        [Test]
+        public void Constructor_ShouldThrow_WhenSettingsNull()
+        {
+            // Arrange
+
+
+            // Act
+
+
+            // Assert
+            Assert.Throws<ArgumentNullException>(() => new RecaptchaV3TagHelper(null, _tagHelperComponentManagerMock.Object));
+        }
+
+        [Test]
+        public void Constructor_ShouldThrow_WhenTagHelperComponentManagerNull()
+        {
+            // Arrange
+
+
+            // Act
+
+
+            // Assert
+            Assert.Throws<ArgumentNullException>(() => new RecaptchaV3TagHelper(_settingsMock.Object, null));
+        }
+
+        [Test]
+        public void Process_ShouldThrow_ArgumentNullException()
+        {
+            // Arrange
+            var scriptTagHelper = new RecaptchaV3TagHelper(_settingsMock.Object, _tagHelperComponentManagerMock.Object);
+
+            // Act
+
+
+            // Assert
+            Assert.Throws<ArgumentNullException>(() => scriptTagHelper.Process(_contextStub, null));
+        }
+
+        [Test]
+        public void Process_ShouldThrow_NullReferenceException_WhenCallbackAndFormIdNullOrEmpty()
+        {
+            // Arrange
+            var nullCallbackTagHelper = new RecaptchaV3TagHelper(_settingsMock.Object, _tagHelperComponentManagerMock.Object)
+            {
+                Callback = null,
+                FormId = null
+            };
+            var emptyCallbackTagHelper = new RecaptchaV3TagHelper(_settingsMock.Object, _tagHelperComponentManagerMock.Object)
+            {
+                Callback = string.Empty,
+                FormId = string.Empty
+            };
+
+            // Act
+
+
+            // Assert
+            Assert.Throws<NullReferenceException>(() => nullCallbackTagHelper.Process(_contextStub, _tagHelperOutputStub));
+            Assert.Throws<NullReferenceException>(() => emptyCallbackTagHelper.Process(_contextStub, _tagHelperOutputStub));
+        }
+
+        [Test]
+        public void Process_ShouldThrow_NullReferenceException_WhenActionIsNullOrEmpty()
+        {
+            // Arrange
+            var nullCallbackTagHelper = new RecaptchaV3TagHelper(_settingsMock.Object, _tagHelperComponentManagerMock.Object)
+            {
+                FormId = "formId",
+                Action = null
+            };
+            var emptyCallbackTagHelper = new RecaptchaV3TagHelper(_settingsMock.Object, _tagHelperComponentManagerMock.Object)
+            {
+                FormId = "formId",
+                Action = String.Empty
+            };
+
+            // Act
+
+
+            // Assert
+            Assert.Throws<NullReferenceException>(() => nullCallbackTagHelper.Process(_contextStub, _tagHelperOutputStub));
+            Assert.Throws<NullReferenceException>(() => emptyCallbackTagHelper.Process(_contextStub, _tagHelperOutputStub));
+        }
+
+        [Test]
+        public void Process_ShouldSet_CallbackToDefaultCallback_WhenCallbackIsNullOrEmpty()
+        {
+            // Arrange
+            var formId = $"formId";
+            var tag = new RecaptchaV3TagHelper(_settingsMock.Object, _tagHelperComponentManagerMock.Object)
+            {
+                Callback = null,
+                FormId = formId,
+                Action = "submit"
+            };
+
+            // Act
+            tag.Process(_contextStub, _tagHelperOutputStub);
+
+            // Assert
+            Assert.AreEqual(tag.Callback, $"submit{formId}");
+        }
+
+        [Test]
+        public void Process_ShouldAdd_CallbackScriptTagHelperComponent_WhenCallbackIsNullOrEmpty()
+        {
+            // Arrange
+            var tag = new RecaptchaV3TagHelper(_settingsMock.Object, _tagHelperComponentManagerMock.Object)
+            {
+                Callback = null,
+                FormId = "formId",
+                Action = "submit"
+            };
+
+            // Act
+            tag.Process(_contextStub, _tagHelperOutputStub);
+
+            // Assert
+            _tagHelperComponentCollectionMock.Verify();
+        }
+
+        [Test]
+        public void Process_ShouldChange_TagModeTo_StartTagAndEndTag()
+        {
+            // Arrange
+            var tagHelper = new RecaptchaV3TagHelper(_settingsMock.Object, _tagHelperComponentManagerMock.Object)
+            {
+                Callback = "submit",
+                Action = "submit"
+            };
+            _tagHelperOutputStub.TagMode = TagMode.SelfClosing;
+
+            // Act
+            tagHelper.Process(_contextStub, _tagHelperOutputStub);
+
+            // Assert
+            Assert.AreEqual(TagMode.StartTagAndEndTag, _tagHelperOutputStub.TagMode);
+        }
+
+        [Test]
+        public void Process_ShouldChangeTag_ToButtonTag()
+        {
+            // Arrange
+            var tagHelper = new RecaptchaV3TagHelper(_settingsMock.Object, _tagHelperComponentManagerMock.Object)
+            {
+                Callback = "submit",
+                Action = "submit"
+            };
+
+            // Act
+            tagHelper.Process(_contextStub, _tagHelperOutputStub);
+
+            // Assert
+            Assert.AreEqual("button", _tagHelperOutputStub.TagName);
+        }
+
+        [Test]
+        public void Process_ShouldAdd_RecaptchaClass()
+        {
+            // Arrange
+            var tagHelper = new RecaptchaV3TagHelper(_settingsMock.Object, _tagHelperComponentManagerMock.Object)
+            {
+                Callback = "submit",
+                Action = "submit"
+            };
+
+            // Act
+            tagHelper.Process(_contextStub, _tagHelperOutputStub);
+
+            // Assert
+            Assert.IsTrue(_tagHelperOutputStub.Attributes.ContainsName("class"));
+            Assert.AreEqual("g-recaptcha", _tagHelperOutputStub.Attributes["class"].Value);
+        }
+
+        [Test]
+        public void Process_ShouldAdd_RecaptchaClassAnd_KeepExistingClasses()
+        {
+            // Arrange
+            var tagHelper = new RecaptchaV3TagHelper(_settingsMock.Object, _tagHelperComponentManagerMock.Object)
+            {
+                Callback = "submit",
+                Action = "submit"
+            };
+            _tagHelperOutputStub.Attributes.Add("class", "container text-center");
+
+            // Act
+            tagHelper.Process(_contextStub, _tagHelperOutputStub);
+            var classes = _tagHelperOutputStub.Attributes["class"].Value.ToString().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            // Assert
+            Assert.AreEqual(3, classes.Length);
+            Assert.IsTrue(classes.Contains("g-recaptcha"));
+            Assert.IsTrue(classes.Contains("text-center"));
+            Assert.IsTrue(classes.Contains("container"));
+        }
+
+        [Test]
+        public void Process_ShouldAdd_SiteKeyAttribute()
+        {
+            // Arrange
+            var tagHelper = new RecaptchaV3TagHelper(_settingsMock.Object, _tagHelperComponentManagerMock.Object)
+            {
+                Callback = "myCallback",
+                Action = "submit"
+            };
+
+            // Act
+            tagHelper.Process(_contextStub, _tagHelperOutputStub);
+
+            // Assert
+            Assert.IsTrue(_tagHelperOutputStub.Attributes.ContainsName("data-sitekey"));
+            Assert.AreEqual(SiteKey, _tagHelperOutputStub.Attributes["data-sitekey"].Value.ToString());
+        }
+
+        [Test]
+        public void Process_ShouldOverrideExisting_SiteKeyAttribute()
+        {
+            // Arrange
+            var tagHelper = new RecaptchaV3TagHelper(_settingsMock.Object, _tagHelperComponentManagerMock.Object)
+            {
+                Callback = "myCallback",
+                Action = "submit"
+            };
+            _tagHelperOutputStub.Attributes.Add("data-sitekey", "false-key");
+
+            // Act
+            tagHelper.Process(_contextStub, _tagHelperOutputStub);
+
+            // Assert
+            Assert.AreEqual(1, _tagHelperOutputStub.Attributes.Count(attribute => attribute.Name == "data-sitekey"));
+            Assert.AreEqual(SiteKey, _tagHelperOutputStub.Attributes["data-sitekey"].Value.ToString());
+        }
+
+        [Test]
+        public void Process_ShouldAdd_DataCallbackAttribute()
+        {
+            // Arrange
+            var tagHelper = new RecaptchaV3TagHelper(_settingsMock.Object, _tagHelperComponentManagerMock.Object)
+            {
+                Callback = "myCallback",
+                Action = "submit"
+            };
+
+            // Act
+            tagHelper.Process(_contextStub, _tagHelperOutputStub);
+
+            // Assert
+            Assert.IsTrue(_tagHelperOutputStub.Attributes.ContainsName("data-callback"));
+            Assert.AreEqual("myCallback", _tagHelperOutputStub.Attributes["data-callback"].Value.ToString());
+        }
+
+        [Test]
+        public void Process_ShouldOverrideExisting_DataCallbackAttribute()
+        {
+            // Arrange
+            var tagHelper = new RecaptchaV3TagHelper(_settingsMock.Object, _tagHelperComponentManagerMock.Object)
+            {
+                Callback = "myCallback",
+                Action = "submit"
+            };
+
+            _tagHelperOutputStub.Attributes.Add("data-callback", "fake-callback");
+
+            // Act
+            tagHelper.Process(_contextStub, _tagHelperOutputStub);
+
+            // Assert
+            Assert.AreEqual(1, _tagHelperOutputStub.Attributes.Count(attribute => attribute.Name == "data-callback"));
+            Assert.AreEqual("myCallback", _tagHelperOutputStub.Attributes["data-callback"].Value.ToString());
+        }
+
+        [Test]
+        public void Process_ShouldAdd_DataActionAttribute()
+        {
+            // Arrange
+            var tagHelper = new RecaptchaV3TagHelper(_settingsMock.Object, _tagHelperComponentManagerMock.Object)
+            {
+                Callback = "myCallback",
+                Action = "submit"
+            };
+
+            // Act
+            tagHelper.Process(_contextStub, _tagHelperOutputStub);
+
+            // Assert
+            Assert.IsTrue(_tagHelperOutputStub.Attributes.ContainsName("data-action"));
+            Assert.AreEqual("submit", _tagHelperOutputStub.Attributes["data-action"].Value.ToString());
+        }
+
+        [Test]
+        public void Process_ShouldOverrideExisting_DataActionAttribute()
+        {
+            // Arrange
+            var tagHelper = new RecaptchaV3TagHelper(_settingsMock.Object, _tagHelperComponentManagerMock.Object)
+            {
+                Callback = "myCallback",
+                Action = "submit"
+            };
+
+            _tagHelperOutputStub.Attributes.Add("data-action", "fake-action");
+
+            // Act
+            tagHelper.Process(_contextStub, _tagHelperOutputStub);
+
+            // Assert
+            Assert.AreEqual(1, _tagHelperOutputStub.Attributes.Count(attribute => attribute.Name == "data-action"));
+            Assert.AreEqual("submit", _tagHelperOutputStub.Attributes["data-action"].Value.ToString());
+        }
+    }
+}
